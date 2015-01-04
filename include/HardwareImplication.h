583baa0be7a467affbe42488d6bbaf98d618c326
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2015-01-04 23:14:42 +0900

    add common include file (indicates hardware implication

---
+++ include/HardwareImplication.h
@@ -0,0 +1,14 @@
+/*
+ * ハードウェアの想定されている状態を定義する
+ */
+
+#ifndef HARDWAREIMPLICATION_H
+#define HARDWAREIMPLICATION_H
+
+// 電源電圧
+#define POWER_SUPPLY_VOLTAGE	5.0
+
+// 動作周波数
+#define OPERATING_FREQUENCY	64000000L
+
+#endif /* HARDWAREIMPLICATION_H */
