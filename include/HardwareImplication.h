45e677a0038d199f6a42df4cf2874424213071ae
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2015-01-07 01:23:31 +0900

    change ifndef guard definition name

--- include/HardwareImplication.h
+++ include/HardwareImplication.h
@@ -2,8 +2,8 @@
  * ハードウェアの想定されている状態を定義する
  */
 
-#ifndef HARDWAREIMPLICATION_H
-#define HARDWAREIMPLICATION_H
+#ifndef HARDWARE_IMPLICATION_H
+#define HARDWARE_IMPLICATION_H
 
 // 電源電圧
 #define POWER_SUPPLY_VOLTAGE	5.0
@@ -11,4 +11,4 @@
 // 動作周波数
 #define OPERATING_FREQUENCY	64000000L
 
-#endif /* HARDWAREIMPLICATION_H */
+#endif /* HARDWARE_IMPLICATION_H */
