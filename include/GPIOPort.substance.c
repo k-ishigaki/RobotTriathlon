de3bf8806f09785a26de705dc3deb29f40a9ccab
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2015-01-25 17:33:32 +0900

    add GPIOPort substance definition source code

---
+++ include/GPIOPort.substance.c
@@ -0,0 +1,46 @@
+/*
+ * GPIOポートの実体生成部
+ * GPIOPortモジュールを利用するソースコードによってインクルードされる
+ */
+
+// 普通のソースコードからはインクルードできないようにする
+#ifdef USING_GPIO_PORT_SUBSTANCE
+
+static void NAMESPACE(setAnalogInput)(unsigned char pattern) {
+	*NAMESPACE(ansel) = pattern;
+	*NAMESPACE(tris) = pattern;
+}
+
+static void NAMESPACE(setAnalogOutput)(unsigned char pattern) {
+	*NAMESPACE(ansel) = pattern;
+	*NAMESPACE(tris) = ~pattern;
+}
+
+static void NAMESPACE(setDigitalInput)(unsigned char pattern) {
+	*NAMESPACE(ansel) = ~pattern;
+	*NAMESPACE(tris) = pattern;
+}
+
+static void NAMESPACE(setDigitalOutput)(unsigned char pattern) {
+	*NAMESPACE(ansel) = ~pattern;
+	*NAMESPACE(tris) = ~pattern;
+}
+
+static unsigned char NAMESPACE(getValue)() {
+	return *NAMESPACE(port);
+}
+
+static void NAMESPACE(setValue)(unsigned char pattern) {
+	*NAMESPACE(lat) = pattern;
+}
+
+static GPIOPort NAMESPACE(gpioPort) = {
+	NAMESPACE(setAnalogInput),
+	NAMESPACE(setAnalogOutput),
+	NAMESPACE(setDigitalInput),
+	NAMESPACE(setDigitalOutput),
+	NAMESPACE(getValue),
+	NAMESPACE(setValue),
+};
+
+#endif /* USING_GPIO_PORT_SUBSTANCE */
