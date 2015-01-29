68129f6713e1051586386e39f1b71518629b9485
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2015-01-29 18:50:51 +0900

    add GPIO Pin define

--- include/Hardware.h
+++ include/Hardware.h
@@ -14,11 +14,11 @@
 // 動作周波数
 #define OPERATING_FREQUENCY	64000000L
 
-// GPIO PORT
+// GPIO Port
 #include "GPIOPort.h"
 /**
  * GPIO PORTのオブジェクトを返す
- * @return GPIO PORTの構造体ポインタ、取得できない場合はNULL
+ * @return GPIO Portの構造体ポインタ、取得できない場合はNULL
  */
 GPIOPort* getPORTA(void);
 GPIOPort* getPORTB(void);
@@ -30,9 +30,44 @@ GPIOPort* getPORTC(void);
  * EUSARTのオブジェクトを返す
  * @return EUSARTの構造体ポインタ，取得できない場合はNULL
  */
-Eusart* getEUSART(void);
+Eusart* getEUSART(void);	// alias of getEUSART1
 Eusart* getEUSART1(void);
 Eusart* getEUSART2(void);
 
+// GPIO Pin
+#include "GPIOPin.h"
+/**
+ * GPIOPinのオブジェクトを返す
+ * @return GPIO Pinの構造体ポインタ，取得できない場合はNULL
+ */
+// PORTA
+GPIOPin* getRA0(void);
+GPIOPin* getRA1(void);
+GPIOPin* getRA2(void);
+GPIOPin* getRA3(void);
+GPIOPin* getRA4(void);
+GPIOPin* getRA5(void);
+GPIOPin* getRA6(void);
+GPIOPin* getRA7(void);
+//PORTB
+GPIOPin* getRB0(void);
+GPIOPin* getRB1(void);
+GPIOPin* getRB2(void);
+GPIOPin* getRB3(void);
+GPIOPin* getRB4(void);
+GPIOPin* getRB5(void);
+GPIOPin* getRB6(void);
+GPIOPin* getRB7(void);
+// PORTC
+GPIOPin* getRC0(void);
+GPIOPin* getRC1(void);
+GPIOPin* getRC2(void);
+GPIOPin* getRC3(void);
+GPIOPin* getRC4(void);
+GPIOPin* getRC5(void);
+GPIOPin* getRC6(void);
+GPIOPin* getRC7(void);
+// PORTE
+GPIOPin* getRE3(void);
 
 #endif /* HARDWARE_H */
