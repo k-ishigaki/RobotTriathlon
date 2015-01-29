bf6c3fc7832aa1df034b20c788fefd5dad6c8e6f
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2015-01-29 23:23:28 +0900

    add interface definition

--- include/GPIOPin.h
+++ include/GPIOPin.h
@@ -1,6 +1,35 @@
 #ifndef GPIO_PIN_H
 #define GPIO_PIN_H
 
+typedef void (*DigitalPin_setDigitalInput)();
+typedef void (*DigitalPin_setDigitalOutput)();
+typedef unsigned char (*DigitalPin_getValue)(void);
+typedef void (*DigitalPin_setValue)(unsigned char value);
 
+typedef void (*AnalogPin_setAnalogInput)();
+typedef void (*AnalogPin_setAnalogOutput)();
+
+typedef struct {
+	DigitalPin_setDigitalInput setDigitalInput;
+	DigitalPin_setDigitalOutput setDigitalOutput;
+	DigitalPin_getValue getValue;
+	DigitalPin_setValue setValue;
+} GPIOPin;
+
+typedef struct {
+	DigitalPin_setDigitalInput setDigitalInput;
+	DigitalPin_setDigitalOutput setDigitalOutput;
+	DigitalPin_getValue getValue;
+	DigitalPin_setValue setValue;
+} DigitalPin;
+
+typedef struct {
+	DigitalPin_setDigitalInput setDigitalInput;
+	DigitalPin_setDigitalOutput setDigitalOutput;
+	DigitalPin_getValue getValue;
+	DigitalPin_setValue setValue;
+	AnalogPin_setAnalogInput setAnalogInput;
+	AnalogPin_setAnalogOutput setAnalogOutput;
+} AnalogPin;
 
 #endif /* GPIO_PIN_H */
