1ee27f0963781479ec78a722db304409023ffd09
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2015-02-06 02:13:00 +0900

    add PLL interface definition

--- include/OscillatorModule.h
+++ include/OscillatorModule.h
@@ -11,23 +11,40 @@
 
 typedef struct {
 	/**
-	 * 内部オシレータの周波数を選択する
-	 * @param 周波数(Hz)
+	 * 内部オシレータの周波数を設定する
+	 * 無効な値を指定した時は何もしない
+	 * @param 周波数(Hz)，いくつかの値に限られる
 	 */
 	void (*setFrequency)(unsigned long frequency);
 } InternalOscillator;
 
 typedef struct {
 	/**
+	 * PLLを有効にする
+	 */
+	void (*enablePLL)(void);
+	/**
+	 * PLLを無効にする
+	 */
+	void (*disablePLL)(void);
+} PhaseLockedLoop;
+
+typedef struct {
+	/**
 	 * モードを選択する
 	 * @param モードを示す列挙子(Hardware.hで定義)
 	 */
-	void (*setMode)(int mode);
+	void (*selectSystemClock)(int mode);
 	/**
 	 * 内部オシレータのオブジェクトを取得する
 	 * @return 内部オシレータのオブジェクト
 	 */
 	InternalOscillator* (*getInternalOscillator)(void);
+	/**
+	 * PLLを設定するオブジェクトを取得する
+	 * @return PLLの設定をするオブジェクト，持っていない場合はNULL
+	 */
+	PhaseLockedLoop* (*getPhaseLockedLoop)(void);
 } OscillatorModule;
 
 #endif /* OSCILLATOR_MODULE_H */
