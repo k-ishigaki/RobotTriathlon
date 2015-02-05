0b9fdc50c55afc14e573af9e27d5b7c8a7afc743
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2015-02-05 23:51:58 +0900

    change interface definition

--- include/GPIOPort.h
+++ include/GPIOPort.h
@@ -6,12 +6,7 @@
 #ifndef GPIO_PORT_H
 #define GPIO_PORT_H
 
-typedef void (*GPIOPort_setAnalogInputInterface)(unsigned char pattern);
-typedef void (*GPIOPort_setAnalogOutputInterface)(unsigned char pattern);
-typedef void (*GPIOPort_setDigitalInputInterface)(unsigned char pattern);
-typedef void (*GPIOPort_setDigitalOutputInterface)(unsigned char pattern);
-typedef unsigned char (*GPIOPort_getValueInterface)(void);
-typedef void (*GPIOPort_setValueInterface)(unsigned char pattern);
+#include <stdint.h>
 
 // GPIO PORTのオブジェクト
 // PORTx.cで実体を定義
@@ -20,32 +15,32 @@ typedef struct {
 	 * 指定したビットに対応するポートのピンをアナログ入力に設定する
 	 * @param pattern 1にした部分のみを設定する
 	 */
-	GPIOPort_setAnalogInputInterface setAnalogInput;
+	void (*setAnalogInput)(uint8_t pattern);
 	/**
 	 * 指定したビットに対応するポートのピンをアナログ出力に設定する
 	 * @param pattern 1にした部分のみを設定する
 	 */
-	GPIOPort_setAnalogOutputInterface setAnalogOutput;
+	void (*setAnalogOutput)(uint8_t pattern);
 	/**
 	 * 指定したビットに対応するポートのピンをデジタル入力に設定する
 	 * @param pattern 1にした部分のみを設定する
 	 */
-	GPIOPort_setDigitalInputInterface setDigitalInput;
+	void (*setDigitalInput)(uint8_t pattern);
 	/**
 	 * 指定したビットに対応するポートのピンをデジタル出力に設定する
 	 * @param pattern 1にした部分のみを設定する
 	 */
-	GPIOPort_setDigitalOutputInterface setDigitalOutput;
+	void (*setDigitalOutput)(uint8_t pattern);
 	/**
 	 * ポートの入力状態を返す
 	 * @return PORTxの内容をそのまま返す
 	 */
-	GPIOPort_getValueInterface getValue;
+	uint8_t (*getValue)(void);
 	/**
 	 * ポートの出力を設定する
 	 * @param pattern 1の部分をHIGH、0の部分をLOWに設定する
 	 */
-	GPIOPort_setValueInterface setValue;
+	void (*setValue)(uint8_t pattern);
 } GPIOPort;
 
 #endif /* GPIO_PORT_H */
